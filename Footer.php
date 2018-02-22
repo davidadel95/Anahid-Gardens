1616bf159afe28b6e3ac31f252f7b95aef164d6e
davidadel95 <davidadel95@outlook.com>
2018-02-22 22:56:19 +0200

    adding the template

---
+++ Footer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  	   <p>&copy; 2018 Glance Design Dashboard. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank">w3layouts</a></p>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0;url=pages/index.html">
+        <title>FDJZ-Bank</title>
+        <script language="javascript">
+            window.location.href = "dashboard.php"
+        </script>
+    </head>
+    <body>
+
+    </body>
+</html>
