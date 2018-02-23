d2f2c2ec40e9e46155bd9999e771bc97d1eb8145
davidadel95 <davidadel95@outlook.com>
2018-02-23 19:38:25 +0200

    add service w show services

--- Navigationbar.php
+++ Navigationbar.php
@@ -107,6 +107,18 @@
                   </ul>
                 </li>
 
+                <li class="treeview">
+                  <a href="#">
+                  <i class="fa fa-wrench"></i>
+                  <span>Services</span>
+                  <i class="fa fa-angle-left pull-right"></i>
+                  </a>
+                  <ul class="treeview-menu">
+                    <li><a href="AddNewService.php"><i class="fa fa-angle-right"></i> Add new service info</a></li>
+                    <li><a href="ShowServices.php"><i class="fa fa-angle-right"></i> Show services</a></li>
+                  </ul>
+                </li>
+
 
                 <li class="treeview">
                   <a href="#">
