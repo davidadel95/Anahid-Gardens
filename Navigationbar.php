65580af5a16b210a3d9be10a9501ddd596210317
davidadel95 <davidadel95@outlook.com>
2018-02-23 19:42:49 +0200

    navbar

--- Navigationbar.php
+++ Navigationbar.php
@@ -52,6 +52,15 @@
                     <span>Home</span>
                   </a>
                 </li>
+
+                <li class="treeview">
+                  <a href="AddFood.php">
+                  <i class="fa fa-yoast"></i>
+                  <span>Add new type of food or drink</span>
+                  </a>
+                </li>
+
+                
                 <!-- kids checklist menu -->
                 <li class="treeview">
                   <a href="#">
@@ -76,23 +85,6 @@
                     </ul>
                     </li>
                   </ul>
-                </li>
-
-
-                <li class="treeview">
-                  <a href="charts.html">
-                  <i class="fa fa-pie-chart"></i>
-                  <span>Charts</span>
-                  <span class="label label-primary pull-right">new</span>
-                  </a>
-                </li>
-
-
-                <li class="treeview">
-                  <a href="AddFood.php">
-                  <i class="fa fa-yoast"></i>
-                  <span>Add new type of food or drink</span>
-                  </a>
                 </li>
 
                 <li class="treeview">
@@ -122,53 +114,6 @@
 
                 <li class="treeview">
                   <a href="#">
-                  <i class="fa fa-laptop"></i>
-                  <span>UI Elements</span>
-                  <i class="fa fa-angle-left pull-right"></i>
-                  </a>
-                  <ul class="treeview-menu">
-                    <li><a href="general.html"><i class="fa fa-angle-right"></i> General</a></li>
-                    <li><a href="icons.html"><i class="fa fa-angle-right"></i> Icons</a></li>
-                    <li><a href="buttons.html"><i class="fa fa-angle-right"></i> Buttons</a></li>
-                    <li><a href="typography.html"><i class="fa fa-angle-right"></i> Typography</a></li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="widgets.html">
-                  <i class="fa fa-th"></i> <span>Widgets</span>
-                  <small class="label pull-right label-info">08</small>
-                  </a>
-                </li>
-                <li class="treeview">
-                  <a href="#">
-                  <i class="fa fa-edit"></i> <span>Forms</span>
-                  <i class="fa fa-angle-left pull-right"></i>
-                  </a>
-                  <ul class="treeview-menu">
-                    <li><a href="forms.php"><i class="fa fa-angle-right"></i> General Forms</a></li>
-                    <li><a href="validation.html"><i class="fa fa-angle-right"></i> Form Validations</a></li>
-                  </ul>
-                </li>
-                <li class="treeview">
-                  <a href="#">
-                  <i class="fa fa-table"></i> <span>Tables</span>
-                  <i class="fa fa-angle-left pull-right"></i>
-                  </a>
-                  <ul class="treeview-menu">
-                    <li><a href="tables.html"><i class="fa fa-angle-right"></i> Simple tables</a></li>
-                  </ul>
-                </li>
-                <li class="treeview">
-                  <a href="#">
-                  <i class="fa fa-envelope"></i> <span>Mailbox </span>
-                  <i class="fa fa-angle-left pull-right"></i><small class="label pull-right label-info1">08</small><span class="label label-primary1 pull-right">02</span></a>
-                  <ul class="treeview-menu">
-                    <li><a href="inbox.html"><i class="fa fa-angle-right"></i> Mail Inbox </a></li>
-                    <li><a href="compose.html"><i class="fa fa-angle-right"></i> Compose Mail </a></li>
-                  </ul>
-                </li>
-                <li class="treeview">
-                  <a href="#">
                   <i class="fa fa-folder"></i> <span>Examples</span>
                   <i class="fa fa-angle-left pull-right"></i>
                   </a>
@@ -180,11 +125,6 @@
                     <li><a href="blank-page.html"><i class="fa fa-angle-right"></i> Blank Page</a></li>
                   </ul>
                 </li>
-                <li class="header">LABELS</li>
-                <li><a href="#"><i class="fa fa-angle-right text-red"></i> <span>Important</span></a></li>
-                <li><a href="#"><i class="fa fa-angle-right text-yellow"></i> <span>Warning</span></a></li>
-                <li><a href="#"><i class="fa fa-angle-right text-aqua"></i> <span>Information</span></a></li>
-              </ul>
             </div>
             <!-- /.navbar-collapse -->
         </nav>
