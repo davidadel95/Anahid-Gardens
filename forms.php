019c8d5b87fff4ff3bea8670438cc2f99146dc5f
john1500463 <john1500463@miuegypt.edu.eg>
2018-02-24 09:08:32 +0200

    bar

--- forms.php
+++ forms.php
@@ -46,7 +46,7 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
 <body class="cbp-spmenu-push">
 	<div class="main-content">
 	<div class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
-	   <?php include("NavigationBar.php"); ?>
+	   <?php include("Navigationbar.php"); ?>
 	</div>
 		<!--left-fixed -navigation-->
 
@@ -370,7 +370,7 @@ SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony
 	</div>
 
 	<!-- side nav js -->
-	
+
 	<!-- //Classie --><!-- //for toggle left push menu script -->
 
 	<!--scrolling js-->
