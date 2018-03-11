49e4aa10ca25495c7b2be242cf9cbc30c4f0dfd9
davidadel95 <davidadel95@outlook.com>
2018-03-11 22:11:02 +0200

    class

--- Admin/Classes/NameEAV.php
+++ Admin/Classes/NameEAV.php
@@ -4,7 +4,7 @@
 /**
  *
  */
-class NameEAV
+class NameEAV implements CRUD
 {
     /**
      *
@@ -24,6 +24,39 @@ class NameEAV
     public $Name;
 
 
+
+
+    /**
+     * @inheritDoc
+     */
+    public function Add():void
+    {
+        // TODO: implement here
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function Edit():void
+    {
+        // TODO: implement here
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function View():void
+    {
+        // TODO: implement here
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function Delete():void
+    {
+        // TODO: implement here
+    }
 
 
 
