40c3eeb765fd2f1cad977a08a5e7bdae3524c410
shaf3t-miu <0123162233m>
2018-05-11 00:16:40 +0200

    Notification/logout icons in header

--- Admin/php/Header.php
+++ Admin/php/Header.php
@@ -7,6 +7,11 @@ if (session_status() == PHP_SESSION_NONE) {
   <head>
     <meta charset="utf-8">
     <!-- Bootstrap Core CSS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+ 
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+ 
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="../css/bootstrap.css" rel='stylesheet' type='text/css' />
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
  
@@ -47,12 +52,29 @@ if (session_status() == PHP_SESSION_NONE) {
 
       <!--search-box-->
         
-          
-
+        
  
-  
+      <a href="logout.php" class="navbar-right user-name" style="padding-right:20px;"  > <span class="glyphicon glyphicon-off" style="font-size:18px;"></span></a>
  
-  
+      
+ 
+    
+        
+        <ul class="nav navbar-nav navbar-right">
+ 
+     <li class="dropdown">
+ 
+      <a href="#"  data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-bell" style="font-size:18px;"></span></a>
+ 
+      <ul class="dropdown-menu"></ul>
+ 
+     </li>
+ 
+    </ul>
+       
+        
+ 
+    
 
       
    
@@ -89,11 +111,11 @@ if (session_status() == PHP_SESSION_NONE) {
                 </div>
                
               </div>
-            
            
           </li>
         </ul>
       </div>
+         
       <div class="clearfix"> </div>
      
     </div>
